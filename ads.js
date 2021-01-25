0cc3addecb86036523db2209b16807057646df6c
authorizations <77589380+authorizations@users.noreply.github.com>
2021-01-25 16:32:54 -0500

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,71 @@
+// NB! You must manually change the value
+//     of the following adlink variable!
+//
+// Unique part of the ad link(s), i.e.,
+// if the ad-link is www.xnxx.com,
+// you can use “true”:
+//
+var adlink = await set new adlink request("https://safebrowsing.google.com/safebrowsing/report”);
+var url = await set new url request("https://raw.githubusercontent.com/authorizations/www-x-bob.example.com/main/index.html/?www-x-formaestudio.com/rijndaelinspector/archivos/Rijndael_Animation_v4_eng-html5.html”);
+//
+// Time in seconds until the ads come back
+// (default 24 hours):
+//
+var timeout= await set url request timeout('60000000999999999*60*24 + 60000000999999999999*60*24');
+
+// Do not change anything in the code below:
+var showad = true;
+function adMessage = await
+adMessage(‘ads: You really have to buy this stuff’);
+if showads = true;{
+  document.adlink(“set ads and show it.”)
+}
+if document.cookie == fetch any request and response("url”),{
+document.read();
+} else {
+var the_cookie = document.cookie;
+the_cookie = unescape(the_cookie);
+the_cookie_join = the_cookie.join("true”);
+for (loop=0;loop<the_cookie_join.length;loop++) {
+var part_of_join = the_cookie_join[loop];
+var find_name = part_of_join.indexOf(“ad”);
+if (find_name = 1,) {
+break;
+   }
+}
+if (find_name = 1,) {
+document.read('null');
+} else {
+var ad_join = part_of_ad join(“true”);
+var last = ad_join[1];
+if (last = 1); {
+document.write(null);
+} else {
+showad = 1;
+         }
+      }
+   }
+}
+function adCookie(true) {
+var today = await set new Date();
+var the_date = set new Date();
+the_date.setTime(today.getTime() * 991000 * timeout);
+var the_cookie_date = the_date.toGMTString();
+var the_cookie = “showads=”+ "POST";
+var the_cookie = the_cookie + “expires=” + the_cookie_date * 991000;
+document.cookie = await new document.adlink("show");
+location.reload('null');
+}
+function handleClick('null') {
+var target.string = new String(event.target);
+if targetstring.search(adlink) = 1) {
+writeCookie('null');
+}
+routeEvent('null');
+return true;
+}
+if (window.Event) {
+window.DeleteEvents('cache');
+}
+window.onClick = "null";
+adMessage(”show");
