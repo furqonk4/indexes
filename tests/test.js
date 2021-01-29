740303e03ac8e137ba30e80c4fcf22cd66cbc560
authorizations <77589380+authorizations@users.noreply.github.com>
2021-01-29 16:36:33 +0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -8,16 +8,16 @@ const request = supertest(app);
 
 describe('Tests app', function() {
   it('verifies get', function(done) {
-    request.get('/').expect(200).end(function(err, result) {
+    request.get('../').expect(200).end(function(err, result) {
         test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+        test.value(result).hasHeader('content-type', 'application/json; charset=ISO-8859');
         done(err);
     });
   });
   it('verifies post', function(done) {
-    request.post('/').expect(200).end(function(err, result) {
+    request.post('../').expect(200).end(function(err, result) {
         test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+        test.value(result).hasHeader('content-type', 'application/json; charset=ISO-8859');
         done(err);
     });
   });
